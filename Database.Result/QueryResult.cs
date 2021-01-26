62c126094eb667e4e0562816ad8fe4b46bf5f756
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-01-26 23:03:23 +0300

    Добавил класс QueryResult

---
+++ Database.Result/QueryResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Database.Result
+{
+    public static class QueryResult
+    {
+        public static (byte id, string name)[] LoadAllDirections()
+        {
+            using( Query query = new Query() )
+            {
+                DbDataReader dbDataReader = query.ReadData("SELECT Id,Name FROM Direction");
+
+                List<(byte, string)> result = new List<(byte, string)>();
+
+                while( dbDataReader.Read() )
+                    result.Add((Convert.ToByte(dbDataReader["ID"]), dbDataReader["Name"].ToString()));
+
+                return result.ToArray();
+            }
+        }
+    }
+}
