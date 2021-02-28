9132fc2f5796cf4ae169beaf00e347f50e9ed5bf
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-02-28 19:53:44 +0300

    Removed sqllite dependencies

--- UnitDatabaseTest/SystemVerifyKnowledgeFixture.cs
+++ UnitDatabaseTest/SystemVerifyKnowledgeFixture.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Data.Common;
-using System.Data.SqlClient;
-using System.Data.SQLite;
 using System.IO;
-using System.Text;
+using Microsoft.Data.SqlClient;
 
 namespace DataBaseTest
 {
