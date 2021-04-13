f63c28beb6989f3e0b9652de843ec050adec0a8b
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-04-13 18:19:15 +0300

    Removed redundant coder

--- SystemVerifyKnowledge.CoreLib/Common/Enum.cs
+++ SystemVerifyKnowledge.CoreLib/Common/Enum.cs
@@ -12,30 +12,6 @@ namespace SystemVerifyKnowledge.CoreLib.Common
             DescriptionAttribute attribute = (DescriptionAttribute)fieldInfo.GetCustomAttribute(typeof(DescriptionAttribute));
             return attribute.Description;
         }
-
-        public static object EnumValueOf(string value, Type enum_type)
-        {
-            string[] names = Enum.GetNames(enum_type);
-            foreach(string name in names)
-            {
-                if(ValueOf((Enum)Enum.Parse(enum_type, name)).Equals(value))
-                    return Enum.Parse(enum_type, name);
-            }
-            throw new ArgumentException("Строка не описана или не задана.");
-        }
-
-        public static string[] CollectionValueOf(Type enum_type)
-        {
-            Array enums = Enum.GetValues(enum_type);
-            string[] values = new string[enums.Length];
-            int i = 0;
-            foreach(object val in enums)
-            {
-                values[i] = ValueOf((Enum)val);
-                i++;
-            }
-            return values;
-        }
     }
 
     public enum Direction
